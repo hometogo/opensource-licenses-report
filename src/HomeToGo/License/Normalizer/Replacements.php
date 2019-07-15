ece1e1acd4061dbdc186885167f292a8004de2b7
Audrius Bugas <3022728+audriusbugas@users.noreply.github.com>
2019-07-15 13:17:38 +0300

    Apache license replacement

--- src/HomeToGo/License/Normalizer/Replacements.php
+++ src/HomeToGo/License/Normalizer/Replacements.php
@@ -5,14 +5,21 @@ namespace HomeToGo\License\Normalizer;
 class Replacements implements NormalizerInterface
 {
 
+    const APACHE2 = 'Apache-2.0';
+    const MIT = 'MIT';
+
     /**
      * @var array
      */
     private $replacements = [
-        'MIT License' => 'MIT',
-        'Apache2' => 'Apache-2.0',
+        'MIT License' => self::MIT,
+        'MIT*' => self::MIT,
+        'Apache2' => self::APACHE2,
         'LGPL Version 3' => 'LGPL-3.0',
-        'Apache 2' => 'Apache-2.0'
+        'Apache 2' => self::APACHE2,
+        'Apache, Version 2.0' => self::APACHE2,
+        'Apache 2.0' => self::APACHE2,
+        'ApacheV2' => self::APACHE2
     ];
 
     /**
